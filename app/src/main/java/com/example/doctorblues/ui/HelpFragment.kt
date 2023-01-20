41f545b7d7e0255e398631e032a0a83e5b59ceae
oki2207 <115452565+oki2207@users.noreply.github.com>
2023-01-20 12:52:03 +0100

    zwischenschritt layout radius Fragments beginn recycle im Layout

--- app/src/main/java/com/example/doctorblues/ui/HelpFragment.kt
+++ app/src/main/java/com/example/doctorblues/ui/HelpFragment.kt
@@ -1,6 +1,34 @@
 package com.example.doctorblues.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.doctorblues.R
+import com.example.doctorblues.databinding.FragmentHelpBinding
+import com.example.doctorblues.databinding.FragmentIllBinding
 
 class HelpFragment: Fragment() {
+    private lateinit var binding: FragmentHelpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_help,
+            container, false
+
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO: CODE
+    }
 }
--- app/src/main/java/com/example/doctorblues/ui/SelftestFragment.kt
+++ app/src/main/java/com/example/doctorblues/ui/SelftestFragment.kt
@@ -1,6 +1,34 @@
 package com.example.doctorblues.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.doctorblues.R
+import com.example.doctorblues.databinding.FragmentIllBinding
+import com.example.doctorblues.databinding.FragmentSelftestBinding
 
 class SelftestFragment:Fragment() {
+    private lateinit var binding: FragmentSelftestBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_selftest,
+            container, false
+
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO: CODE
+    }
 }
